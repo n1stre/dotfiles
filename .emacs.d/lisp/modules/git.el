f1950aff384d8d36715fb18a1bc29e352c8641be
streletss <xstreletssx@gmail.com>
2026-03-28 15:06:25 +0100

    feat(git): git gutter setup

--- .emacs.d/lisp/modules/git.el
+++ .emacs.d/lisp/modules/git.el
@@ -1,4 +1,36 @@
 (use-package magit) ;; best git UI in Emacs
 
 
+(use-package git-gutter
+  :hook (prog-mode . git-gutter-mode)
+  :config
+  (setq
+   git-gutter:update-interval 2
+   git-gutter:hide-gutter -1
+   git-gutter:always-show-separator t
+   git-gutter:margin-left 1
+   )
+
+  (custom-set-variables
+   '(git-gutter:modified-sign "=")
+   '(git-gutter:added-sign "+")
+   '(git-gutter:deleted-sign "-")
+   '(git-gutter:unchanged-sign " ")
+   '(git-gutter:window-width git-gutter:margin-left)
+   )
+
+  ;; (set-face-foreground 'git-gutter:modified "purple")
+  ;; (set-face-foreground 'git-gutter:added "#eeeeee")
+  ;; (set-face-foreground 'git-gutter:deleted "red")
+  
+  (defun git-gutter-set-margins ()
+    "Set margins in current buffer."
+    (setq left-margin-width git-gutter:margin-left)  
+    (set-window-buffer nil (current-buffer))
+    )
+  
+  (add-hook 'prog-mode-hook 'git-gutter-set-margins)
+  
+  )
+
 (provide 'git)
